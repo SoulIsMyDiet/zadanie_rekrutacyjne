d8f4c27d3cce5e3336a1fd580d65f5abdf6c394f
Anzelm <marekd.90@gmail.com>
2023-09-30 15:17:27 +0200

    add some orm packages; made entity

---
+++ zadanie_rekrutacyjne/src/Entity/History.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\HistoryRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=HistoryRepository::class)
+ */
+class History
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $firstIn;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $secondIn;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $firstOut;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $secondOut;
+
+    /**
+     * @ORM\Column(type="datetime_immutable")
+     */
+    private $createdAt;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $updatedAt;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFirstIn(): ?int
+    {
+        return $this->firstIn;
+    }
+
+    public function setFirstIn(int $firstIn): self
+    {
+        $this->firstIn = $firstIn;
+
+        return $this;
+    }
+
+    public function getSecondIn(): ?int
+    {
+        return $this->secondIn;
+    }
+
+    public function setSecondIn(int $secondIn): self
+    {
+        $this->secondIn = $secondIn;
+
+        return $this;
+    }
+
+    public function getFirstOut(): ?int
+    {
+        return $this->firstOut;
+    }
+
+    public function setFirstOut(int $firstOut): self
+    {
+        $this->firstOut = $firstOut;
+
+        return $this;
+    }
+
+    public function getSecondOut(): ?int
+    {
+        return $this->secondOut;
+    }
+
+    public function setSecondOut(int $secondOut): self
+    {
+        $this->secondOut = $secondOut;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(\DateTimeInterface $updatedAt): self
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+}
